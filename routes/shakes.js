d5457375947e7a8c1bba1a27bcc03b8991d165aa
Sai Sharma <msaisharma9@gmail.com>
2021-11-30 17:28:22 -0600

    added changes

--- routes/shakes.js
+++ routes/shakes.js
@@ -2,6 +2,15 @@ var express = require('express');
 const shakes_controlers= require('../controllers/shakes');
 var router = express.Router();
 
+// redirect to login. 
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+} 
+
 /* GET shakes */ 
 router.get('/', shakes_controlers.shakes_view_all_Page );
 module.exports = router;
@@ -15,13 +24,13 @@ router.get('/', function(req, res, next) {
 router.get('/detail', shakes_controlers.shakes_view_one_Page); 
 
 /* GET create shakes page */ 
-router.get('/create', shakes_controlers.shakes_create_Page); 
+router.get('/create',secured, shakes_controlers.shakes_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', shakes_controlers.shakes_update_Page); 
+router.get('/update',secured, shakes_controlers.shakes_update_Page); 
 
 /* GET create shakes page */ 
-router.get('/delete', shakes_controlers.shakes_delete_Page); 
+router.get('/delete',secured, shakes_controlers.shakes_delete_Page); 
 
 module.exports = router;
 
