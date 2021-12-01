ecb42048e195d80146a5aae829a0206d1a6fbefc
Sai Sharma <msaisharma9@gmail.com>
2021-11-30 18:32:19 -0600

    added changes

--- models/shakes.js
+++ models/shakes.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const shakesSchema = mongoose.Schema({
-    flavour: String,
-    size: String,
+    flavour:{
+        type: String,
+        minLength: 10
+    }, 
+     size: {
+        type: String,
+        minLength: 11
+     },
+
     cost: String
 })
 module.exports = mongoose.model("shakes", shakesSchema)
